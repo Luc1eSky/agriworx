e7a3a9c35c7ea20dbf180236c8fb195fc80d2a23
Lucie <schulz.lucie@gmail.com>
2024-08-04 00:05:04 -0700

    update default dialog

--- lib/common_widgets/default_dialog.dart
+++ lib/common_widgets/default_dialog.dart
@@ -7,46 +7,73 @@ class DefaultDialog extends StatelessWidget {
     super.key,
     required this.title,
     required this.child,
+    required this.hasCloseButton,
   });
 
   final String title;
   final Widget child;
+  final bool hasCloseButton;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(dialogPadding),
-          child: Material(
-            color: Colors.transparent,
-            child: Container(
-              constraints: const BoxConstraints(
-                minWidth: dialogMinSize,
-                maxWidth: dialogMaxWidth,
-                minHeight: dialogMinSize,
-              ),
-              decoration: BoxDecoration(
-                color: ColorPalette.card,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(title, style: const TextStyle(fontSize: 24)),
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(dialogPadding),
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  constraints: const BoxConstraints(
+                    minWidth: dialogMinSize,
+                    maxWidth: dialogMaxWidth,
+                    minHeight: dialogMinSize,
+                  ),
+                  decoration: BoxDecoration(
+                    color: ColorPalette.card,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(title, style: const TextStyle(fontSize: 24)),
+                        ),
+                        const SizedBox(height: 20),
+                        child,
+                      ],
                     ),
-                    const SizedBox(height: 20),
-                    child,
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+          if (hasCloseButton)
+            Positioned(
+              right: 10,
+              top: 10,
+              child: CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 20,
+                child: FittedBox(
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.close,
+                      size: 36,
+                    ),
+                    onPressed: () {
+                      print('POP');
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
