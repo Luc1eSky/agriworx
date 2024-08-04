267eb4937ef3db45e424e7947400ff322e67c5a2
Lucie <schulz.lucie@gmail.com>
2024-08-04 13:28:43 -0700

    - update firestore rules to allow access till end of 2024 - remove enum converter as this is automatically done by json serializable - add build.yaml file to enforce the rule that the child's .toJson() and .fromJson() methods have to be called by default

--- lib/features/soil_and_round/domain/soil.dart
+++ lib/features/soil_and_round/domain/soil.dart
@@ -10,7 +10,7 @@ part 'soil.g.dart';
 class Soil with _$Soil {
   const Soil._();
   const factory Soil({
-    @SoilTypeConverter() required SoilType type,
+    required SoilType type,
     //required Color color,
     required String colorDescription,
     required String imageString,
@@ -25,34 +25,10 @@ class Soil with _$Soil {
     final soil = possibleSoils.firstWhere((soil) => soil.type == soilType);
     return soil;
   }
-  //
-  // @override
-  // factory Soil.fromJson(Map<String, Object?> json) {
-  //   final soilTypeString = json['type'] as String;
-  //   final soil = possibleSoils.firstWhere((soil) => soil.type.name == soilTypeString);
-  //   return soil;
-  // }
-  //
-  // Map<String, dynamic> _$SoilToJson(Soil instance) => {'type': type.name};
-
-  //Map<String, dynamic> toJson() => {'type': type.name};
 }
 
 enum SoilType {
   grey,
   brown,
   black,
-}
-
-class SoilTypeConverter implements JsonConverter<SoilType, String> {
-  const SoilTypeConverter();
-
-  @override
-  SoilType fromJson(String string) {
-    final type = SoilType.values.firstWhere((value) => value.name == string);
-    return type;
-  }
-
-  @override
-  String toJson(SoilType soilType) => soilType.name;
 }
